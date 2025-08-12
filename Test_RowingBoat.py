89de71ab4a9a10a8293fde99ed31abbdf2cf9dad
technoborsch <75335319+technoborsch@users.noreply.github.com>
2025-08-12 11:10:39 +0300

    Additional test cases, additional automated tests

--- Test_RowingBoat.py
+++ Test_RowingBoat.py
@@ -166,11 +166,11 @@ def test_reverse_movement(standard_boat):
 
 def test_rotation_boundaries(standard_boat):
     """TC-FUNC-03: Проверка граничных значений rotation"""
-    standard_boat.rotation = -1.1
-    assert standard_boat.rotation == -1.0
+    standard_boat.rotation = -90.1
+    assert standard_boat.rotation == -90.0
 
-    standard_boat.rotation = 33
-    assert standard_boat.rotation == 1.0
+    standard_boat.rotation = 333
+    assert standard_boat.rotation == 90.0
 
 
 def test_rowing_rate_changes(standard_boat):
@@ -182,3 +182,372 @@ def test_rowing_rate_changes(standard_boat):
     standard_boat.rotation = 0.8
     left2, right2 = standard_boat.rowing_rate
     assert left2 < 0  # Отрицательное значение означает обратную греблю
+
+
+def test_max_weight_0_is_impossible():
+    """TC-FUNC-05: Невозможность создать лодку с максимальным весом 0"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=0,
+            weight=0,
+            number_of_rowers=4,
+            max_rowing_frequency=2.0,
+            max_speed=10.0,
+            max_rotation=90.0,
+            rower_force=150.0
+        )
+
+
+def test_weight_acceleration_impact(standard_boat):
+    """TC-FUNC-06: Влияние веса на ускорение"""
+    light_boat = standard_boat
+    heavy_boat = RowingBoat(
+        max_weight=1000,
+        weight=600,  # Перегруз
+        number_of_rowers=4,
+        max_rowing_frequency=2.0,
+        max_speed=10.0,
+        max_rotation=90.0,
+        rower_force=150.0
+    )
+
+    light_boat.acceleration = 1.0
+    light_boat.move(1.0)
+    heavy_boat.acceleration = 1.0
+    heavy_boat.move(1.0)
+
+    assert light_boat.speed > heavy_boat.speed
+
+
+def test_acceleration_boundaries(standard_boat):
+    """TC-FUNC-07: Проверка граничных условий acceleration"""
+    standard_boat.acceleration = -1.6
+    acceleration1 = standard_boat.acceleration
+    standard_boat.acceleration = 1000
+    acceleration2 = standard_boat.acceleration
+
+    assert acceleration1 == -1.0
+    assert acceleration2 == 1.0
+
+
+def test_direction_360_degrees_passing(standard_boat):
+    """TC-FUNC-08: Проверка прохождения direction через 360 градусов"""
+    standard_boat.rotation = 359
+    standard_boat.move(1.0)
+    direction1 = standard_boat.direction
+    standard_boat.rotation = 2
+    direction2 = standard_boat.direction
+
+    assert direction1 == 359
+    assert direction2 == 1
+
+
+def test_no_movement_without_rowers():
+    """TC-FUNC-09: Без гребцов лодка не движется"""
+    no_rowers_boat = RowingBoat(
+        max_weight=500,
+        weight=300,
+        number_of_rowers=0, # Ноль тут
+        max_rowing_frequency=2.0,
+        max_speed=10.0,
+        max_rotation=90.0,
+        rower_force=150.0
+    )
+
+    no_rowers_boat.acceleration = 1.0
+    no_rowers_boat.move(1.0)
+
+    assert no_rowers_boat.speed == 0
+    assert no_rowers_boat.position[0] == 0
+    assert no_rowers_boat.position[1] == 0
+
+
+def test_no_movement_with_zero_max_rowing_frequency():
+    """TC-FUNC-10: С предельной частотой гребков 0 лодка не движется"""
+    no_rowing_boat = RowingBoat(
+        max_weight=500,
+        weight=300,
+        number_of_rowers=4,
+        max_rowing_frequency=0, # Ноль тут
+        max_speed=10.0,
+        max_rotation=90.0,
+        rower_force=150.0
+    )
+
+    no_rowing_boat.acceleration = 1.0
+    no_rowing_boat.move(1.0)
+
+    assert no_rowing_boat.speed == 0
+    assert no_rowing_boat.position[0] == 0
+    assert no_rowing_boat.position[1] == 0
+
+def test_no_movement_with_zero_max_speed():
+    """TC-FUNC-11: С предельной скоростью 0 лодка не движется"""
+    no_speed_boat = RowingBoat(
+        max_weight=500,
+        weight=300,
+        number_of_rowers=4,
+        max_rowing_frequency=2.0,
+        max_speed=0, # Ноль тут
+        max_rotation=90.0,
+        rower_force=150.0
+    )
+
+    no_speed_boat.acceleration = 1.0
+    no_speed_boat.move(1.0)
+
+    assert no_speed_boat.speed == 0
+    assert no_speed_boat.position[0] == 0
+    assert no_speed_boat.position[1] == 0
+
+
+def test_no_rotation_without_rowers():
+    """TC-FUNC-12: Без гребцов лодка не вращается"""
+    no_rowers_boat = RowingBoat(
+        max_weight=500,
+        weight=300,
+        number_of_rowers=0,  # Ноль тут
+        max_rowing_frequency=2.0,
+        max_speed=10.0,
+        max_rotation=90.0,
+        rower_force=150.0
+    )
+
+    no_rowers_boat.rotation = 1.0
+    no_rowers_boat.move(1.0)
+
+    assert no_rowers_boat.direction == 0
+
+
+def test_no_movement_with_zero_rower_force():
+    """TC-FUNC-13: С предельной силой гребцов 0 лодка не движется"""
+    no_force_boat = RowingBoat(
+        max_weight=500,
+        weight=300,
+        number_of_rowers=4,
+        max_rowing_frequency=2.0,
+        max_speed=10.0,
+        max_rotation=90.0,
+        rower_force=0.0  # Ноль тут
+    )
+
+    no_force_boat.acceleration = 1.0
+    no_force_boat.move(1.0)
+
+    assert no_force_boat.speed == 0
+    assert no_force_boat.position[0] == 0
+    assert no_force_boat.position[1] == 0
+
+
+def test_no_rotation_with_zero_rower_force():
+    """TC-FUNC-14: С предельной силой гребцов 0 лодка не вращается"""
+    no_force_boat = RowingBoat(
+        max_weight=500,
+        weight=300,
+        number_of_rowers=4,
+        max_rowing_frequency=2.0,
+        max_speed=10.0,
+        max_rotation=90.0,
+        rower_force=0.0  # Ноль тут
+    )
+
+    no_force_boat.rotation = 1.0
+    no_force_boat.move(1.0)
+
+    assert no_force_boat.direction == 0
+
+
+def test_impossible_initialization_with_negative_max_weight():
+    """TC-FUNC-16: Невозможность создать лодку с отрицательным max_weight"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=-1,
+            weight=300,
+            number_of_rowers=4,
+            max_rowing_frequency=2.0,
+            max_speed=10.0,
+            max_rotation=90.0,
+            rower_force=150.0
+        )
+
+
+def test_impossible_initialization_with_negative_weight():
+    """TC-FUNC-17: Невозможность создать лодку с отрицательным weight"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=500,
+            weight=-1,
+            number_of_rowers=4,
+            max_rowing_frequency=2.0,
+            max_speed=10.0,
+            max_rotation=90.0,
+            rower_force=150.0
+        )
+
+
+def test_impossible_initialization_with_negative_number_of_rowers():
+    """TC-FUNC-18: Невозможность создать лодку с отрицательным number_of_rowers"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=500,
+            weight=300,
+            number_of_rowers=-1,
+            max_rowing_frequency=2.0,
+            max_speed=10.0,
+            max_rotation=90.0,
+            rower_force=150.0
+        )
+
+
+def test_impossible_initialization_with_negative_max_rowing_frequency():
+    """TC-FUNC-19: Невозможность создать лодку с отрицательным max_rowing_frequency"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=500,
+            weight=300,
+            number_of_rowers=4,
+            max_rowing_frequency=-1,
+            max_speed=10.0,
+            max_rotation=90.0,
+            rower_force=150.0
+        )
+
+
+def test_impossible_initialization_with_negative_max_speed():
+    """TC-FUNC-20: Невозможность создать лодку с отрицательным max_speed"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=500,
+            weight=300,
+            number_of_rowers=4,
+            max_rowing_frequency=2.0,
+            max_speed=-1,
+            max_rotation=90.0,
+            rower_force=150.0
+        )
+
+
+def test_impossible_initialization_with_negative_max_rotation():
+    """TC-FUNC-21: Невозможность создать лодку с отрицательным max_rotation"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=500,
+            weight=300,
+            number_of_rowers=4,
+            max_rowing_frequency=2.0,
+            max_speed=10.0,
+            max_rotation=-1,
+            rower_force=150.0
+        )
+
+
+def test_impossible_initialization_with_negative_rower_force():
+    """TC-FUNC-22: Невозможность создать лодку с отрицательным rower_force"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=500,
+            weight=300,
+            number_of_rowers=4,
+            max_rowing_frequency=2.0,
+            max_speed=10.0,
+            max_rotation=90.0,
+            rower_force=-1
+        )
+
+
+def test_impossible_initialization_with_string_max_weight():
+    """TC-FUNC-16: Невозможность создать лодку со строковым max_weight"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight="a",
+            weight=300,
+            number_of_rowers=4,
+            max_rowing_frequency=2.0,
+            max_speed=10.0,
+            max_rotation=90.0,
+            rower_force=150.0
+        )
+
+
+def test_impossible_initialization_with_string_weight():
+    """TC-FUNC-17: Невозможность создать лодку со строковым weight"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=500,
+            weight="a",
+            number_of_rowers=4,
+            max_rowing_frequency=2.0,
+            max_speed=10.0,
+            max_rotation=90.0,
+            rower_force=150.0
+        )
+
+
+def test_impossible_initialization_with_string_number_of_rowers():
+    """TC-FUNC-18: Невозможность создать лодку со строковым number_of_rowers"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=500,
+            weight=300,
+            number_of_rowers="a",
+            max_rowing_frequency=2.0,
+            max_speed=10.0,
+            max_rotation=90.0,
+            rower_force=150.0
+        )
+
+
+def test_impossible_initialization_with_string_max_rowing_frequency():
+    """TC-FUNC-19: Невозможность создать лодку со строковым max_rowing_frequency"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=500,
+            weight=300,
+            number_of_rowers=4,
+            max_rowing_frequency="a",
+            max_speed=10.0,
+            max_rotation=90.0,
+            rower_force=150.0
+        )
+
+
+def test_impossible_initialization_with_string_max_speed():
+    """TC-FUNC-20: Невозможность создать лодку со строковым max_speed"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=500,
+            weight=300,
+            number_of_rowers=4,
+            max_rowing_frequency=2.0,
+            max_speed="a",
+            max_rotation=90.0,
+            rower_force=150.0
+        )
+
+
+def test_impossible_initialization_with_string_max_rotation():
+    """TC-FUNC-21: Невозможность создать лодку со строковым max_rotation"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=500,
+            weight=300,
+            number_of_rowers=4,
+            max_rowing_frequency=2.0,
+            max_speed=10.0,
+            max_rotation="a",
+            rower_force=150.0
+        )
+
+
+def test_impossible_initialization_with_string_rower_force():
+    """TC-FUNC-22: Невозможность создать лодку со строковым rower_force"""
+    with pytest.raises(ValueError):
+        _ = RowingBoat(
+            max_weight=500,
+            weight=300,
+            number_of_rowers=4,
+            max_rowing_frequency=2.0,
+            max_speed=10.0,
+            max_rotation=90.0,
+            rower_force="a"
+        )
